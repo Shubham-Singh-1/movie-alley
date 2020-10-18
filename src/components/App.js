5682a7d22e04c3de80cc7e028a8e812070f07fff
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-10-19 02:04:32 +0530

    add action creators

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions'
 
 class App extends React.Component {
 
@@ -16,10 +17,7 @@ class App extends React.Component {
       this.forceUpdate();
     });
     
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data,
-    }); 
+    store.dispatch(addMovies(data)); 
 
     console.log('STATE',this.props.store.getState());
   }
