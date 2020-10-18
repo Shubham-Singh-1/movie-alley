0185690287dd1c550c8e4b0e00045fc9a95e2a2b
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-10-18 16:40:24 +0530

    Add Redux to the project to use store,action and reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import movies from './reducers/index'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(movies);
+
+console.log('store',store);
+
+console.log('BEFORE STATE',store.getState());
+
+
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: [{ name: 'Superman'}]
+});
+
+console.log('AFTER STATE',store.getState());
+
+ReactDOM.render( <App />, document.getElementById('root'));
