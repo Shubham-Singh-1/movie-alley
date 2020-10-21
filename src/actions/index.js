74d37c9e657c25c7f94001845147307589d2cd74
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-10-21 09:50:13 +0530

    Add movie to favourite and Unfavourite

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 //action types
 export const ADD_MOVIES = 'ADD_MOVIES';
-export const ADD_FAVOURITE = 'ADD_FAVOURITE';
+export const ADD_TO_FAVOURITE = 'ADD_FAVOURITE';
+export const REMOVE_FROM_FAVOURITE = 'REMOVE_FROM_FAVOURITE';
 
 //action creators
 export  function addMovies(movies) {
@@ -12,7 +13,14 @@ export  function addMovies(movies) {
 
 export function addFavourite(movie) {
     return {
-        type: ADD_FAVOURITE,
+        type: ADD_TO_FAVOURITE,
+        movie,
+    }
+}
+
+export function removeFromFavourites (movie) {
+    return {
+        type: REMOVE_FROM_FAVOURITE,
         movie,
     }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_MOVIES,ADD_FAVOURITE } from '../actions';
+import { ADD_MOVIES,ADD_TO_FAVOURITE, REMOVE_FROM_FAVOURITE } from '../actions';
 
 const initialMovieState = {
     list: [],
@@ -20,10 +20,18 @@ export default function movies (state = initialMovieState,action) {
                 ...state,
                 list: action.movies,
             }
-        case ADD_FAVOURITE:
+        case ADD_TO_FAVOURITE:
             return {
                 ...state,
                 favourites: [action.movie, ...state.favourites]
+            }    
+        case REMOVE_FROM_FAVOURITE:
+            const filteredArray = state.favourites.filter(
+                movie  => movie.Title !== action.movie.Title,
+            );
+            return {
+                ...state,
+                favourite: filteredArray,
             }    
         default:
             return state;    
