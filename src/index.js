c1b49b8e0af9333956abb9ffb69b0a842ae7700f
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-10-23 06:08:14 +0530

    Add middlewares to show action logs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 import './index.css';
 import App from './components/App';
-import rootReducer from './reducers/index'
+import rootReducer from './reducers/index';
 
-const store = createStore(rootReducer);
+// function logger(obj,dispatch,getState)
+// logger(obj)(next)(action)
+
+// const logger = function( { dispatch,getState} ) {
+//     return function (next) {
+//         return function (action) {
+//             // middleware code
+//             console.log("ACTION TYPE=",action.type);
+//             next(action);
+//         }
+//     }
+// }
+
+const logger = ( {dispatch,getState} ) => (next) => (action) => {
+    console.log("ACTION TYPE:",action.type);
+    next(action);
+}
+
+
+const store = createStore(rootReducer,applyMiddleware(logger));
 
 console.log('store',store);
 
